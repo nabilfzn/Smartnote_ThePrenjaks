fe73f841694e56fe1356ab2c9049f2213e4dbdd7
nabilfzn <140621593+nabilfzn@users.noreply.github.com>
2025-05-16 16:48:39 +0700

    Delete app.py

--- app.py
+++
@@ -1,8 +0,0 @@
-from app import create_app
-import os
-
-app = create_app()
-
-if __name__ == '__main__':
-    print(f"Server started. Recordings will be saved to {os.path.abspath('static')}")
-    app.run(host='0.0.0.0', port=5055, debug=True)
